acaec039bfe49c5aa6552448fdf5c8d6c486810a
erdhjfg <121865822+erdhjfg@users.noreply.github.com>
2023-01-30 13:11:14 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,8 @@
+pins.setAudioPin(AnalogPin.P16)
+for (let index = 0; index < 4; index++) {
+    music.playMelody("F D F D F D F D ", 1000)
+    basic.pause(2000)
+}
+basic.forever(function () {
+	
+})
